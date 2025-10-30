737d1fef55fb84c8251f8458f554f45bc88294f3
OmarOrtVal <23308060610674@cetis61.edu.mx>
2025-10-29 18:30:16 -0700

    trabajo

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
-from flask import Flask, render_template, url_for, request, redirect, flash, session
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/peso/<altura>/<edad>/<genero>/<nivel_de_actividad>')
-def registrar(peso, altura, edad, genero, nivel_de_actividad):
-    print("--- Nuevo Registro ---")
-    print(f"Peso: {peso}")
-    print(f"Altura: {altura}")
-    print(f"Edad: {edad}")
-    print(f"Género: {genero}")
-    print(f"Nivel de Actividad: {nivel_de_actividad}")
-    print("----------------------")
+FACTORES_ACTIVIDAD = {
+    "sedentario": 1.2,
+    "ligera": 1.375,
+    "moderada": 1.55,
+    "alta": 1.725
+}
 
-
+@app.route('/', methods=['GET', 'POST'])
+def calculadora_get():
+    tmb = None
+    get = None
     
+    if request.method == 'POST':
+            peso = float(request.form['peso']) 
+            altura = float(request.form['altura']) 
+            edad = int(request.form['edad']) 
+            genero = request.form['genero'] 
+            actividad = request.form['actividad'] 
+
+            if genero == 'hombre':
+                tmb = (10 * peso) + (6.25 * altura) - (5 * edad) + 5
+            elif genero == 'mujer':
+                tmb = (10 * peso) + (6.25 * altura) - (5 * edad) - 161
+
+            factor_actividad = FACTORES_ACTIVIDAD.get(actividad)
+            
+            if factor_actividad:
+                get = tmb * factor_actividad
+                
+    return render_template('formulario.html', tmb=tmb, get=get)
 
 if __name__ == '__main__':
     app.run(debug=True)
